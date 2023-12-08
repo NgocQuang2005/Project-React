440cc4d6f8e3659d45d471cc034053946113fa38
Quang <Quang111420@gmail.com>
2023-12-08 08:30:16 +0700

    chỉnh sửa

--- src/Component/ContentHome/BodyIndex.tsx
+++ src/Component/ContentHome/BodyIndex.tsx
@@ -1,34 +1,36 @@
 import { IoCall } from "react-icons/io5";
-import Suachua from "./img/category-img-1.jpg"
-const BodyIndex =()=>{
-    return(
-    <div className='max-w-screen-xl mx-auto flex my-5 container '>
-      <div>
-          <img width={580} height={397} src={Suachua} alt="" />
+import Suachua from "../img/category-img-1.jpg";
+
+const BodyIndex = () => {
+  return (
+    <div className='max-w-screen-xl mx-auto flex flex-col-reverse md:flex-row my-5 container'>
+      <div className='md:w-1/2 md:pr-8'>
+        <img className='w-full h-auto' src={Suachua} alt="Service Image" />
       </div>
-      <div className='pl-[50px] py-[50px] '>
+      <div className='md:w-1/2 pl-0 md:pl-[50px] py-5'>
         <div className='font-bold text-orange-500 text-xl'>
-            Our Services
+          Our Services
         </div>
-        <div className='font-bold text-4xl leading-loose font-mono '>
-            Responsive & Professional
-        </div>
-        <div className='max-w-xl text-base text-gray-500'>
-            We go the extra mile on every project. 
-            The value we provide clients comes from our level of skill and performance, as well as our knowledge and professionalism. 
-            Rest assured, we put the same level of energy into every project we take on.
-        </div>
-        <div className='pt-[60px] font-bold'>
-            Call us today
-        </div>
-        <div className='text-orange-500 flex items-center text-2xl font-bold '>
-            <IoCall /> <div>1900-111000</div> 
+        <div className='font-bold text-4xl leading-loose font-mono'>
+          Responsive & Professional
         </div>
         <div className='max-w-xl text-base text-gray-500'>
-            We're available 24/7, 365 days a year.
+          We go the extra mile on every project. 
+          The value we provide clients comes from our level of skill and performance, as well as our knowledge and professionalism. 
+          Rest assured, we put the same level of energy into every project we take on.
         </div>
+        <div className='pt-8 font-bold'>
+          Call us today
         </div>
+        <div className='text-orange-500 flex items-center text-2xl font-bold'>
+          <IoCall /> <div className='ml-2'>1900-111000</div> 
+        </div>
+        <div className='max-w-xl text-base text-gray-500'>
+          We're available 24/7, 365 days a year.
+        </div>
+      </div>
     </div>
-    )
-}
-export default BodyIndex
+  );
+};
+
+export default BodyIndex;
--- src/Component/ContentHome/WhatOffer.tsx
+++ src/Component/ContentHome/WhatOffer.tsx
@@ -1,27 +1,28 @@
-import Offer from "../Inheritance/Offer"
-import anh1 from '../img/lighting-upgrades-thumb-G3.jpg'
-import anh2 from '../img/electrical-repairs-thumb-G1.jpg'
-import anh3 from '../img/surge-protection-thumb-G4.jpg'
+import Offer from "../Inheritance/Offer";
+import anh1 from '../img/lighting-upgrades-thumb-G3.jpg';
+import anh2 from '../img/electrical-repairs-thumb-G1.jpg';
+import anh3 from '../img/surge-protection-thumb-G4.jpg';
 
-const WhatOffer=()=>{
-    return(
-      <div className=' px-[50px] py-[50px] container'>
-        <div className='text-center text-4xl font-bold pb-[20px]'>What We Offer</div>
-        <ul className='max-w-screen-xl mx-auto flex  justify-between'>
-          <li>
-            <img src={anh1} alt="" />
-            <Offer title='Lighting Upgrades' desc='Electrical panel maintenance, therefore, should be part of your regular routine.'/>
-          </li>
-          <li>
-            <img src={anh2} alt="" />
-            <Offer title='Electrical repairs' desc='Electrical repairs should always be handled by a professional electrician.'/>
-          </li>
-          <li>
-            <img src={anh3} alt="" />
-            <Offer title='Surge Protection' desc='An electrical surge can happen for a number of reasons, including lightning strikes..'/>
-          </li>
-        </ul>
+const WhatOffer = () => {
+  return (
+    <div className='py-10 container mx-auto'>
+      <div className='text-center text-4xl font-bold pb-5'>What We Offer</div>
+      <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+        <li>
+          <img src={anh1} alt="Lighting Upgrades" className='w-full h-auto' />
+          <Offer title='Lighting Upgrades' desc='Electrical panel maintenance, therefore, should be part of your regular routine.'/>
+        </li>
+        <li>
+          <img src={anh2} alt="Electrical repairs" className='w-full h-auto' />
+          <Offer title='Electrical repairs' desc='Electrical repairs should always be handled by a professional electrician.'/>
+        </li>
+        <li>
+          <img src={anh3} alt="Surge Protection" className='w-full h-auto' />
+          <Offer title='Surge Protection' desc='An electrical surge can happen for a number of reasons, including lightning strikes..' />
+        </li>
+      </ul>
     </div>
-    )
-}
-export default WhatOffer
+  );
+};
+
+export default WhatOffer;
